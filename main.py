7a060c2a6a2b0d10b199e647583f7adc78aded8f
mjh316 <61671361+mjh316@users.noreply.github.com>
2022-02-24 01:12:03 -0800

    python file

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from pprint import pprint as pp
+from bs4 import BeautifulSoup
+
+url = 'https://www.cnn.com/europe/live-news/ukraine-russia-news-02-23-22/index.html'
+
+
+text = requests.get(url).text
+soup = BeautifulSoup(text, features='lxml')
+
+headers = soup.select("h2.sc-dfVpRl.kvaBeP")
+#'<h2 class="sc-dfVpRl kvaBeP">'
+headers = list(map(lambda x: x.text, headers[1:]))
+
+with open('data.data', 'w') as f:
+    f.write('\n'.join(headers))
+
