bab319e42cf3d324cc392ab18c106ec1a9eaab3c
mjh316 <61671361+mjh316@users.noreply.github.com>
2022-02-25 12:49:30 -0800

    auto parsing

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import requests
 import json
+import pytz
+from datetime import datetime
 from pprint import pprint as pp
 from bs4 import BeautifulSoup
 
-url = 'https://www.cnn.com/europe/live-news/ukraine-russia-news-02-25-22/index.html'
+base_url = 'https://www.cnn.com/europe/live-news/ukraine-russia-news-{}-{}-{}{}/index.html'
 
+tz_WA = pytz.timezone('US/Pacific')
+datetime_WA = datetime.now(tz_WA)
 
-text = requests.get(url).text
+url1 = base_url.format(
+    str(datetime_WA.month).rjust(2, '0'),
+    str(datetime_WA.day).rjust(0, '2'),
+    str(datetime_WA.year)[-2:],
+    '-intl'
+)
+
+url2 = base_url.format(
+    str(datetime_WA.month).rjust(2, '0'),
+    str(datetime_WA.day).rjust(0, '2'),
+    str(datetime_WA.year)[-2:],
+    ''
+)
+#print(url1 + '\n' + url2)
+
+text = requests.get(url1).text
 soup = BeautifulSoup(text, features='lxml')
 
 headers = soup.select("h2.sc-dfVpRl.kvaBeP")
+if headers == []:
+    text = requests.get(url2).text
+    soup = BeautifulSoup(text, features='lxml')
+    headers = soup.select("h2.sc-dfVpRl.kvaBeP")
 #'<h2 class="sc-dfVpRl kvaBeP">'
 headers = list(map(lambda x: x.text, headers[1:]))
+
+#print(headers)
 
 with open('data.data', 'w') as f:
     f.write('\n'.join(headers))
